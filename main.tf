8ed21b187b44551f00920b9e17d6eee95586f58b
pavan teja <ramanapavanteja04@gmail.com>
2026-05-12 20:12:19 +0530

    terraform files added

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "digi_instance" {
 
     ami = "ami-08399392f867d1d1f"
     instance_type = "t3.micro"
+    key_name = "nit"
+    subnet_id = aws_subnet.pavan_vpc_subnet.id
+    vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
     tags = {
       Name = "pavan-system"
@@ -9,4 +12,8 @@ resource "aws_instance" "digi_instance" {
   
 }
 
+
+module "vpc" {
+  source = "./vpc"
+}
 
